74ca666f4079150657f28e6f3a4c557ee54cd2b7
Tyler Chadwick <chadwickthebold@gmail.com>
2015-01-14 22:06:06 -0500

    imported backbone-boilerplate

--- webapp/js/controllers.js
+++
@@ -1,7 +0,0 @@
-var asmApp = angular.module('asmApp', []);
-
-asmApp.controller('ArtistListControl', function ($scope, $http) {
-	$http.get('json/artistSample.json').success(function(data) {
-		$scope.artists = data;
-	});
-});
