61f0bb9faa67c645267fc31f4e134316ad780b1c
Tyler Chadwick <chadwickthebold@gmail.com>
2015-03-05 21:31:31 -0500

    added src js files

---
+++ musicmapper/static/js/src/footer.jsx
@@ -0,0 +1,17 @@
+// Application namespace
+var asm = asm || {};
+
+(function() {
+	'use strict';
+
+	asm.Footer = React.createClass({
+		render : function() {
+
+			return (
+				<footer>
+					<span>Created by <a href="http://tylerchadwick.com">Tyler Chadwick</a></span>
+				</footer>
+			);
+		}
+	})
+})();
